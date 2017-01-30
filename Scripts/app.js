95c027636142d4c9e3541fa35dd5a56bb26bcddd
Emdaisy1 <hilborn_emmav@live.ca>
2017-01-30 15:18:32 -0500

    Basic logic for setting up the appropriate numbers (1 or 2) based on what the user has clicked.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    //Set up variables
     var numOne;
     var numTwo;
     var op;
@@ -7,31 +8,55 @@ $(document).ready(function(){
     var opSet = false;
     var $currentDisplay = $('#calcDisplay');
 
+    //Set up reset function for Clear button
     function resetCalc(){
-        numOne = null;
-        numTwo = null;
-        op = null;
+        numOne = '';
+        numTwo = '';
+        op = '';
         numOneSet = false;
         $currentDisplay.val('0');
     }
 
+    //"Reset" calculator on start
     resetCalc();
 
+    //Behaviour for handling number & decimal buttons
     $( '.number' ).click( function(){
-        var num = this.id;
-        console.log('Number ' + num + ' clicked');
+        var addToNum = this.id;
+        console.log(op + ' ' + numOne + ' ' + numTwo);
+        //If an operator has been clicked but number 1 has not been set, then set it as number two
+        if( op !== '' && numOne === '' ){
+            numOne = '0';
+            numTwo = numTwo + addToNum;
+            console.log( '1: ' + numOne + ' 2: ' + numTwo);
+        }
+        //If an operator has been clicked, and number 1 is not null/has been set, then set it as number two
+        else if( op !== '' && numOne !== '' ){
+            numTwo = numTwo + this.id;
+            console.log( '1: ' + numOne + ' 2: ' + numTwo);
+        }
+        //If neither of the above, set the number to number one
+        else{
+            numOne = numOne + this.id;
+            console.log( '1: ' + numOne + ' 2: ' + numTwo);
+        }
     });
 
+    //Behaviour for handling operator buttons
     $( '.operator' ).click( function(){
         console.log('operator clicked');
+        op = this.id;
     });
 
-    $( '.decimal' ).click( function(){
-        console.log('Dec clicked');
-    });
-
+    //Behaviour for handling equals button
     $( '.equals' ).click( function(){
         console.log('Equals clicked');
+    });
+
+    //Behaviour for handling clear button
+    $( '.clear' ).click( function(){
+        console.log('Clear clicked');
+        resetCalc();
     });
 
 });
